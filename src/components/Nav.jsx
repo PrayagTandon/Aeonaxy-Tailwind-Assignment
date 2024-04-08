34634dd17d3f55f92559597eb75734b380ca5ba4
Prayag Tandon <tandonprayag99@gmail.com>
2024-04-08 23:38:50 +0530

    ...

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,18 +3,22 @@ import Hamburger from '../assets/images/Hamburger.svg';
 
 const Nav = () => {
     return (
-        <header className='padding-x py-8 absolute z-10 w-full'>
+        <header className='padding-x py-8 z-10 w-full'>
             <nav className='flex justify-between items-center max-container'>
                 <a href="/">
                     <img src={HeaderLogo} alt="Header Logo" width={130} height={29} />
                 </a>
                 <ul className='flex-1 flex justify-center items-center ga-16 max-lg:hidden'>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Individuals</a></li>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Teams</a></li>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Enterprise</a></li>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Product</a></li>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Pricing</a></li>
-                    <li><a href="#" className='font-montserrat leading-normal text-lg text-black font-semibold'>Resources</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold pr-16'>Individuals</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold pr-16'>Teams</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold pr-16'>Enterprise</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold pr-16'>Product</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold pr-16'>Pricing</a></li>
+                    <li><a href="#" className='font-montserrat leading-normal text-md text-black font-semibold'>Resources</a></li>
+                </ul>
+                <ul className='flex-1 flex justify-center items-center ga-16 max-lg:hidden'>
+                    <button type="submit">Log In</button>
+                    <button type="submit">Get Started</button>
                 </ul>
             </nav>
         </header>
